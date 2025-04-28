a74835f87f56bfb7d8e5cb0a8d041a0d63359c72
Emmanuel Lopez <emlopezdev@gmail.com>
2025-04-28 13:27:29 -0400

    created disabled class and created initial js file

---
+++ main.js
@@ -0,0 +1,8 @@
+const emailInput = document.getElementById("email");
+const subscribeButton = document.getElementById("subscribe");
+const dismissButton = document.getElementById("dismiss");
+
+// subscribeButton.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     console.log(emailInput.value);
+// });
